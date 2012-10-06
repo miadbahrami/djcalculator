eb8d188d589394af599e7db5de1747d5d95e82e8
Amin Oruji <aminpy@gmail.com>
2012-10-06 22:46:55 +0330

    add ajax request

--- djcalculator/views.py
+++ djcalculator/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.http import HttpResponse
 
 
 def index(request):
+    if request.is_ajax():
+        return HttpResponse(eval(request.POST["result"]))
+
     return render_to_response('index.html', context_instance=RequestContext(request))
--- media/js/djcalculator.js
+++ media/js/djcalculator.js
@@ -1,3 +1,27 @@
+$.ajaxSetup({ 
+     beforeSend: function(xhr, settings) {
+         function getCookie(name) {
+             var cookieValue = null;
+             if (document.cookie && document.cookie != '') {
+                 var cookies = document.cookie.split(';');
+                 for (var i = 0; i < cookies.length; i++) {
+                     var cookie = jQuery.trim(cookies[i]);
+                     // Does this cookie string begin with the name we want?
+                 if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                     break;
+                 }
+             }
+         }
+         return cookieValue;
+         }
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     } 
+});
+
 $(document).ready(function() {
 	$("#1").click(function() {
 		$("#result").val($("#result").val() + $("#1").val());
@@ -38,4 +62,35 @@ $(document).ready(function() {
 	$("#0").click(function() {
 		$("#result").val($("#result").val() + $("#0").val());
 	});
+	
+	$("#sum").click(function() {
+		$("#result").val($("#result").val() + $("#sum").val());
+	});
+	
+	$("#sub").click(function() {
+		$("#result").val($("#result").val() + $("#sub").val());
+	});
+	
+	$("#mul").click(function() {
+		$("#result").val($("#result").val() + $("#mul").val());
+	});
+	
+	$("#div").click(function() {
+		$("#result").val($("#result").val() + $("#div").val());
+	});
+});
+
+$(document).ready(function() {
+	$("#equal").click(function() {
+		$.ajax({
+			type: "POST",
+			url: "",
+			data: {
+				"result": $("#result").val(),
+			},
+			success: function(result) {
+				$("#result").val(result);
+			},
+		});
+	})
 });
