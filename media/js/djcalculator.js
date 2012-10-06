87057cc47fdade768667d1b6200dbc97aeceaff7
Amin Oruji <aminpy@gmail.com>
2012-10-06 16:13:49 +0330

    improve input data

--- media/js/djcalculator.js
+++ media/js/djcalculator.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
 	$("#1").click(function() {
-		$("#result").val($("#1").val());
+		$("#result").val($("#result").val() + $("#1").val());
+	});
+	
+	$("#2").click(function() {
+		$("#result").val($("#result").val() + $("#2").val());
+	});
+	
+	$("#3").click(function() {
+		$("#result").val($("#result").val() + $("#3").val());
+	});
+	
+	$("#4").click(function() {
+		$("#result").val($("#result").val() + $("#4").val());
+	});
+	
+	$("#5").click(function() {
+		$("#result").val($("#result").val() + $("#5").val());
+	});
+	
+	$("#6").click(function() {
+		$("#result").val($("#result").val() + $("#6").val());
+	});
+	
+	$("#7").click(function() {
+		$("#result").val($("#result").val() + $("#7").val());
+	});
+	
+	$("#8").click(function() {
+		$("#result").val($("#result").val() + $("#8").val());
+	});
+	
+	$("#9").click(function() {
+		$("#result").val($("#result").val() + $("#9").val());
+	});
+	
+	$("#0").click(function() {
+		$("#result").val($("#result").val() + $("#0").val());
 	});
 });
